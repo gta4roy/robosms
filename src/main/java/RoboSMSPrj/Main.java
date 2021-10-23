12674ca7849405e83e7b40ab3fdaf15f04e6731e
Abhijit Roy <gta4roy@gmail.com>
2021-10-23 20:39:55 +0530

    Removed Unneccessary files

--- src/main/java/RoboSMSPrj/Main.java
+++ src/main/java/RoboSMSPrj/Main.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package RoboSMSPrj;
-import com.robo.sms.calculator.EllipticalIntegral;
+
 import com.robo.sms.model.CordinatePoints;
 import com.robo.sms.model.ScullModelElliptical;
 import com.robo.sms.ui.mainUI;
@@ -19,16 +19,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("ROBO SMS Skull Measurement System");
-           /* Create and display the form */
-        http://www.geocities.ws/web_sketches/ellipse_notes/ellipse_arc_length/ellipse_arc_length.html
-//        double b = 2.23;
-//        double a = 3.05;
-//        double angle = Math.toRadians(50);
-//        System.out.println("Arc Length : --- ");
-//        
-//        double arcLength = EllipticalIntegral.GetArcLengthOfEllipse(a, b, angle);
-//        System.out.println("Arc Length : --- "+ arcLength);
-        
+           /* Create and display the form */      
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
